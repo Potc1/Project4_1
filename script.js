68fd3ebeaf9223f1676d0277bd8711aa96dcda5b
Potc1 <andreyi4vaska@gmail.com>
2025-05-03 19:56:49 +0300

    with charts

--- script.js
+++ script.js
@@ -2,16 +2,42 @@ function SetData(data){
     $('#container').html(function(){
         var len = Object.keys(data).length;
         console.log(len)
-        elem = ``;
+        elem = `<h3> Акции </h3>
+                <br> 
+                <hr align="center" width="100%"/> `;
         for(let key in data['Shares']){
-            console.log(data['Shares'][key])
-            elem += `<p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample${data['Shares'][key]['ISIN']}" role="button" aria-expanded="false" aria-controls="collapseExample">${data['Shares'][key]['NAME']}</a></p>` +
+            elem += `<p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample${data['Shares'][key]['ISIN']}" onclick="event.preventDefault(); MakeChart('${data['Shares'][key]['ISIN']}', 'Shares');" role="button" aria-expanded="false" aria-controls="collapseExample">${data['Shares'][key]['NAME']}</a></p>` +
             `<div class="collapse" id="collapseExample${data['Shares'][key]['ISIN']}">
                 <div class="card card-body">
-                ISIN: ${data['Shares'][key]['ISIN']} <br> OPEN: ${data['Shares'][key]['OPEN']} <br> LAST: ${data['Shares'][key]['LAST']} 
+                <b>ISIN:</b> ${data['Shares'][key]['ISIN']} <br> <b>OPEN:<b> ${data['Shares'][key]['OPEN']} LOW: ${data['Shares'][key]['LOW']} <br> <b>LAST:<b> ${data['Shares'][key]['LAST']} HIGH: ${data['Shares'][key]['HIGH']}
                 </div>
-            </div>`  
+                    <div style="max-width: 100%; height: 450px; margin: auto" id="plot${data['Shares'][key]['ISIN']}" class="js-plotly-plot"></div>
+                <button type="button" class="btn btn-primary btn">Добавить</button>
+            </div>` 
+            
         }
+        elem += `<h3> Облигации </h3></hr> 
+                <br> 
+                <hr align="center" width="100%"/> `;
+        for(let key in data['Bonds']){
+            elem += `<p><a class="btn btn-primary" data-toggle="collapse" href="#collapseExample${data['Bonds'][key]['ISIN']}" role="button" aria-expanded="false" aria-controls="collapseExample">${data['Bonds'][key]['NAME']}</a></p>` +
+               `<div class="collapse" id="collapseExample${data['Bonds'][key]['ISIN']}">
+                 <div class="card card-body">
+                ISIN: ${data['Bonds'][key]['ISIN']} <br> <br> OPEN: ${data['Bonds'][key]['OPEN']} <br> LAST: ${data['Bonds'][key]['LAST']} 
+                </div> 
+             </div>` 
+        }
+        
         return elem; 
     })
+}
+
+function MakeChart(ISIN, type){
+    fetch(`https://script.google.com/macros/s/AKfycbzYbVQKlcIVXaqDP2ZpvSoVMs80_KbRX4r1cSdR4mtgy6YXIufTUs-vFlIijFNnM4Jbgg/exec?type=${type}&isin=${ISIN}`, {
+        method : "GET"
+    })
+    .then(response => response.json())
+    .then((data) =>  Plotly.newPlot(`plot${ISIN}`, data['data'], data['layout']))
+    //console.log(chart)
+   
 }
